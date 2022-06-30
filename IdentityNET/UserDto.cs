5505dd7a6c1c897551bdd024abbd071c409058dd
Tien Bui <tien.bui@nois.vn>
2022-06-30 15:18:30 +0700

    Add project files.

---
+++ IdentityNET/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityNET
+{
+    public class UserDto
+
+    { 
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
